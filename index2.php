77efc0fb42895e5a0ddae3dac31c05033196e451
Jackson Killian <killian.34@osu.edu>
2017-07-23 00:51:39 +0000

    Set up php include for headers and footers

---
+++ index2.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <link rel=stylesheet href='/css/animations.css' />
+    <title>BDAA</title>
+    </head>
+    <body>
+        <?php include("nav.html");?>
+        <div class="col-sm-8 col-lg-8 fadein">
+            <div class='jumbotron main-content'>
+                <div class='container padded'>
+                    <div class="row padded">
+                        <h1 class="">News</h1>
+                        <h2><b>Networking Night Fall 2017</b></h2>
+                        <p>Join us for our Networking night on September 25 in the Performance Hall of the Ohio Union.</p>
+
+                        <h2><b>BDAA Brunch</b></h2>
+                        <p>Don't miss us at the BDAA brunch at ethyl!</p>
+                    </div>
+                </div>
+            </div>
+            <div class='jumbotron main-content'>
+                <div class='container padded'>
+                    <h1 class="pad-left">Events</h1>
+                    <br />
+                    <iframe id="gcal" class="calendar-frame" allowtransparency="true" src="cal.php"></iframe>
+                </div>
+            </div>
+        <?php include("footer.html");?>
+            
+        </div>
+        <div class="col-sm-2 col-lg-2">
+        </div>
+    </body>
+</html>
